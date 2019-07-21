8be8bdee0859ab42766072c64eb3360d19d8fc4a
Ash <ashdoes.dev@gmail.com>
2019-07-21 01:47:30 -0700

    Move timing message

--- dist/src/Embeds/application-start.embed.js
+++ dist/src/Embeds/application-start.embed.js
@@ -5,7 +5,7 @@ class ApplicationStartEmbed extends discord_js_1.RichEmbed {
     constructor() {
         super();
         this.setColor('#60b5bc');
-        this.addField('About the Application Process', 'The application has 12 questions in total. Be mindful that it will time out after 30 minutes of inactivity. Please reply as thoroughly as needed, but in one message per question.');
+        this.addField('About the Application Process', 'The application has 12 questions in total and should take about 15 minutes. Be mindful that it will time out after 30 minutes of inactivity. Please reply as thoroughly as needed, but in one message per question.');
         this.addField('Ready?', 'Check yes to begin.');
     }
 }
