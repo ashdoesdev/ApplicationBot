609a7a82e4795d5bd74006e15a59ca1bc6f0cc30
Ash <ashdoes.dev@gmail.com>
2019-07-27 19:30:32 -0700

    Add schedule messaging

--- src/Embeds/intro.embed.ts
+++ src/Embeds/intro.embed.ts
@@ -5,6 +5,6 @@ export class IntroEmbed extends RichEmbed {
         super();
 
         this.setColor('#60b5bc');
-        this.setDescription('Hey there! Thanks for your interest in applying to Sharp and Shiny. Before starting, have you read our <#562899440952934403>?');
+        this.setDescription('Hey there! Thanks for your interest in applying to Sharp and Shiny. Before starting, have you read our <#562899440952934403> and does our raid <#562785022059347978> work for you?');
     }
 }
