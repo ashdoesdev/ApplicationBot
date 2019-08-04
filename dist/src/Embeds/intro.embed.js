1935e98486aa6b21c439bfd401c743df1ed1ecc2
Ash <ashdoes.dev@gmail.com>
2019-08-04 14:45:19 -0700

    Cleanup

--- dist/src/Embeds/intro.embed.js
+++ dist/src/Embeds/intro.embed.js
@@ -5,7 +5,7 @@ class IntroEmbed extends discord_js_1.RichEmbed {
     constructor(charterChannel, scheduleChannel) {
         super();
         this.setColor('#60b5bc');
-        this.setDescription(`Hey there! Thanks for your interest in applying to Sharp and Shiny. Before starting, have you read our <#${charterChannel}> and does our raid <#${scheduleChannel}> work for you ?`);
+        this.setDescription(`Hey there! Thanks for your interest in applying to Sharp and Shiny. Before starting, have you read our <#${charterChannel}> and does our raid <#${scheduleChannel}> work for you?`);
     }
 }
 exports.IntroEmbed = IntroEmbed;
--- src/Embeds/intro.embed.ts
+++ src/Embeds/intro.embed.ts
@@ -5,6 +5,6 @@ export class IntroEmbed extends RichEmbed {
         super();
 
         this.setColor('#60b5bc');
-        this.setDescription(`Hey there! Thanks for your interest in applying to Sharp and Shiny. Before starting, have you read our <#${charterChannel}> and does our raid <#${scheduleChannel}> work for you ?`);
+        this.setDescription(`Hey there! Thanks for your interest in applying to Sharp and Shiny. Before starting, have you read our <#${charterChannel}> and does our raid <#${scheduleChannel}> work for you?`);
     }
 }
