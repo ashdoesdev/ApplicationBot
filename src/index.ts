b26b02761cf889b57d490691d0b032b320c92d46
Ash <ashdoes.dev@gmail.com>
2021-02-27 13:17:25 -0800

    Misc cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { ApplicationBot } from './application-bot';
 import * as appSettings from '../appSettings.prod.json';
 
-const attendanceBot = new ApplicationBot();
-attendanceBot.start(appSettings);
+const applicationBot = new ApplicationBot();
+applicationBot.start(appSettings);
