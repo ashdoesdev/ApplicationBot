c25e1be68aa087dfc2b07aa758d61f414d1312a1
Ash <ashdoes.dev@gmail.com>
2021-02-27 16:49:19 -0800

    Unstage ignored files

--- dist/src/Models/ApplicationState.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ApplicationState {
-    constructor() {
-        this.replies = new Array();
-    }
-}
-exports.ApplicationState = ApplicationState;
--- dist/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const application_bot_1 = require("./application-bot");
-const appSettings = require("../appSettings.dev.json");
-const applicationBot = new application_bot_1.ApplicationBot();
-applicationBot.start(appSettings);
